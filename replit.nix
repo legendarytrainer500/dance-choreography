dbf69d5b07ab372bbc8813eff60625950e7e0fa0
legendarytrainer500 <18grakestraw@gmail.com>
2024-11-03 15:55:21 -0600

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,4 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python39
-    pkgs.python39Packages.pip
-    pkgs.python39Packages.PySimpleGUI
-    pkgs.python39Packages.python-docx
-  ];
-}
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.python39Packages.pip
+pkgs.python39Packages.PySimpleGUI 
+pkgs.python39Packages.python-docx
